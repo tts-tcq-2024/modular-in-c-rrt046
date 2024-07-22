92548793a91049a1669481be290fe9bc5b7a7787
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-22 16:54:00 +0530

    Update ColorToNumber.c

--- ColorToNumber.c
+++ ColorToNumber.c
@@ -2,17 +2,6 @@
 #include <string.h>
 #include "ColorPair.h"
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-const int NUMBER_OF_MAJOR_COLORS = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const int NUMBER_OF_MINOR_COLORS = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
 int GetPairNumberFromColorNames(const char* majorColorName, const char* minorColorName) {
     int majorColorIndex = -1;
     int minorColorIndex = -1;
