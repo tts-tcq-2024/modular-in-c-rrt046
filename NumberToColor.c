4d47949580add57e414c74c8725e8b0c5113646d
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-22 16:53:43 +0530

    Update NumberToColor.c

--- NumberToColor.c
+++ NumberToColor.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 #include "ColorPair.h"
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-const int NUMBER_OF_MAJOR_COLORS = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const int NUMBER_OF_MINOR_COLORS = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
 void GetColorNamesFromPairNumber(int pairNumber, char* majorColorName, char* minorColorName) {
     if (pairNumber < 1 || pairNumber > NUMBER_OF_MAJOR_COLORS * NUMBER_OF_MINOR_COLORS) {
         printf("Error: Pair number out of range.\n");
