f6bf61ecfe2a7a25ac2f50b0d518f63246550fd1
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-22 15:13:49 +0530

    Update ColorPair.c

--- ColorPair.c
+++ ColorPair.c
@@ -10,21 +10,33 @@ const char* MinorColorNames[] = {
 };
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
+const int NUMBER_OF_MAJOR_COLORS = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const int NUMBER_OF_MINOR_COLORS = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / 5);
-    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % 5);
+    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / NUMBER_OF_MINOR_COLORS);
+    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % NUMBER_OF_MINOR_COLORS);
     return colorPair;
 }
 
 int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * 5 + colorPair->minorColor + 1;
+    return colorPair->majorColor * NUMBER_OF_MINOR_COLORS + colorPair->minorColor + 1;
 }
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
+}
+
+void PrintColorCodingManual(void) {
+    printf("Color Coding Reference Manual:\n");
+    for (int i = 1; i <= NUMBER_OF_MAJOR_COLORS * NUMBER_OF_MINOR_COLORS; i++) {
+        ColorPair colorPair = GetColorFromPairNumber(i);
+        char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+        ColorPairToString(&colorPair, colorPairNames);
+        printf("Pair Number %d: %s\n", i, colorPairNames);
+    }
 }
