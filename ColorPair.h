099be98fa0962677f5ef20c53064927abc6c3e16
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-22 15:10:27 +0530

    Create ColorPair.h

---
+++ ColorPair.h
@@ -0,0 +1,20 @@
+#ifndef COLOR_PAIR_H
+#define COLOR_PAIR_H
+
+enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern const int MAX_COLORPAIR_NAME_CHARS;
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+
+#endif // COLOR_PAIR_H
