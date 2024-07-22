cd2ac07f10493f39d2cf2239e33ee551a06008a0
Remalli Ravi Teja <106062789+rrt046@users.noreply.github.com>
2024-07-22 15:01:03 +0530

    Create ColorPairConversions.c

---
+++ ColorPairConversions.c
@@ -0,0 +1,16 @@
+#include "ColorPair.h"
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+int GetPairNumberFromColor(const ColorPair* colorPair) {
+    return colorPair->majorColor * numberOfMinorColors +
+            colorPair->minorColor + 1;
+}
